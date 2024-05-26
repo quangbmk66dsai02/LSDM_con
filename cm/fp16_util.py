3892bff336f3db223c0b44937115b50b138b4710
quangbmk66dsai02 <128770333+quangbmk66dsai02@users.noreply.github.com>
2024-05-26 15:29:59 +0700

    Update fp16_util.py

--- cm/fp16_util.py
+++ cm/fp16_util.py
@@ -43,6 +43,8 @@ def make_master_params(param_groups_and_shapes):
     cnt = 0
     for param_group, shape in param_groups_and_shapes:
         print(cnt)
+        print("this is len param",len(param_group))
+        print("this is shape", shape)
         master_param = nn.Parameter(
             _flatten_dense_tensors(
                 [param.detach().float() for (_, param) in param_group]
