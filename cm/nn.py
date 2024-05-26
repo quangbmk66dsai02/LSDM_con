950c13eb775a26d849da12675393442174166ca3
quangbmk66dsai02 <128770333+quangbmk66dsai02@users.noreply.github.com>
2024-05-26 08:22:08 +0700

    Update nn.py

--- cm/nn.py
+++ cm/nn.py
@@ -65,6 +65,9 @@ def update_ema(target_params, source_params, rate=0.99):
     """
     if len(target_params) == len(source_params):
         print(f"Current len param is equal ={len(source_params)} source parameters")
+    print(target_params[0])
+    print("============")
+    print(source_params[0])
     if len(target_params) != len(source_params):
         print(f"Parameter count mismatch: {len(target_params)} target parameters vs {len(source_params)} source parameters")
     if len(target_params) > len(source_params):
